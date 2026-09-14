31d1877998bccf50b165af70f9543c2a12e40fce
Jaeil An <a090066@naver.com>
2026-09-15 00:49:14 +0900

    fix: 설정 시간대 서울로 변경

--- src/main/java/com/travel/meomulkyung/itinerary/service/ItineraryTimeConfiguration.java
+++ src/main/java/com/travel/meomulkyung/itinerary/service/ItineraryTimeConfiguration.java
@@ -4,11 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Clock;
+import java.time.ZoneId;
 
 @Configuration
 public class ItineraryTimeConfiguration {
     @Bean
     Clock itineraryClock() {
-        return Clock.systemDefaultZone();
+
+        return Clock.system(ZoneId.of("Asia/Seoul"));
     }
 }
